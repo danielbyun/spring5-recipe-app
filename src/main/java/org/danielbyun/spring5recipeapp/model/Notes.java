ae4dd1d62a2ca43afd705d9717c291e84946d1a8
Daniel Byun <dbyun@doinglab.com>
2019-07-04 13:37:07 +0900

    Fix circular reference with hashCode method on entities.

--- src/main/java/org/danielbyun/spring5recipeapp/model/Notes.java
+++ src/main/java/org/danielbyun/spring5recipeapp/model/Notes.java
@@ -1,10 +1,12 @@
 package org.danielbyun.spring5recipeapp.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
     @Id
