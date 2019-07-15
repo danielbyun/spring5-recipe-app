408755535a146145c1991f744e80ef591f5c5a1f
Daniel Byun <dbyun@doinglab.com>
2019-07-15 10:05:07 +0900

    show recipe by id. bug: lazy initialization added more testing.

--- src/main/java/org/danielbyun/spring5recipeapp/controller/RecipeController.java
+++ src/main/java/org/danielbyun/spring5recipeapp/controller/RecipeController.java
@@ -15,7 +15,7 @@ public class RecipeController {
     }
 
     @RequestMapping("/recipe/show/{id}")
-    private String showById(@PathVariable String id, Model model) {
+    public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
